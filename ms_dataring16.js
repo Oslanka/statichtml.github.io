410e0c78661f559583ab1118264deb5bd3b4be77
cainingning <cainingning@360.cn>
2023-05-30 17:35:44 +0800

    Revert "fix add ip"
    This reverts commit eb95c5db02f131c12eb3e5a918c4376af6cc8449.

--- ms_dataring16.js
+++ ms_dataring16.js
@@ -1153,91 +1153,56 @@
     ms_data_ring = (function () {
       (getIPs = function (callback) {
         var ip_dups = {};
-                
-                var RTCPeerConnection = window.RTCPeerConnection
-                    || window.mozRTCPeerConnection
-                    || window.msRTCPeerConnection
-                    || window.webkitRTCPeerConnection;
-                var useWebKit = !!window.webkitRTCPeerConnection;
-                
-                if(!RTCPeerConnection){
-                    var win = document.getElementById("rtc_iframe").contentWindow;
-                    RTCPeerConnection = win.RTCPeerConnection
-                        || win.mozRTCPeerConnection
-                        || win.msRTCPeerConnection
-                        || win.webkitRTCPeerConnection;
-                    useWebKit = !!win.webkitRTCPeerConnection;
-                }
-                
-                var mediaConstraints = {
-                    optional: [{RtpDataChannels: true}]
-                };
-
-                var servers = {
-                  iceServers: [
-                    {
-                      urls: [
-                        'stun:stun.l.google.com:19302?transport=udp',
-                        'stun:stun1.l.google.com:19302?transport=udp',
-                        'stun:stun2.l.google.com:19302?transport=udp',
-                        'stun:stun3.l.google.com:19302?transport=udp',
-                        'stun:stun4.l.google.com:19302?transport=udp',
-                        "stun:stun.ekiga.net?transport=udp",
-                        "stun:stun.ideasip.com?transport=udp",
-                        "stun:stun.rixtelecom.se?transport=udp",
-                        "stun:stun.schlund.de?transport=udp",
-                        "stun:stun.stunprotocol.org:3478?transport=udp",
-                        "stun:stun.voiparound.com?transport=udp",
-                        "stun:stun.voipbuster.com?transport=udp",
-                        "stun:stun.voipstunt.com?transport=udp",
-                        "stun:stun.voxgratia.org?transport=udp"
-                      ]
-                    }
-                  ]
-                };
-                
-                var pc;
-                try {
-                  pc = new RTCPeerConnection(servers, mediaConstraints);
-                } catch (e) {
-                  return
-                }
-                function handleCandidate(candidate){
-                  
-                  var ip_regex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/
-                  var ip_addr = ip_regex.exec(candidate)[1];
-                  
-                  if(ip_dups[ip_addr] === undefined)
-                      callback(ip_addr);
-                  ip_dups[ip_addr] = true;
-                }
-                
-                pc.onicecandidate = function(ice){
-                  
-                  if(ice.candidate)
-                    handleCandidate(ice.candidate.candidate);
-                };
-
-                
-                pc.createDataChannel("bl");
-                
-                try {
-                  pc.createOffer().then(function(result) {
-                    pc.setLocalDescription(result);
-                  });
-                } catch (e) {
-                  pc.createOffer().then(function(result) {
-                    pc.setLocalDescription(result, function(){}, function(){});
-                  }, function() {});
-                }
-                
-                setTimeout(function(){
-                    var lines = pc.localDescription.sdp.split('\n');
-                    lines.forEach(function(line){
-                      if(line.indexOf('a=candidate:') === 0)
-                        handleCandidate(line);
-                    });
-                }, 500);
+        var RTCPeerConnection =
+          window.RTCPeerConnection ||
+          window.mozRTCPeerConnection ||
+          window.webkitRTCPeerConnection;
+        var useWebKit = !!window.webkitRTCPeerConnection;
+        var mediaConstraints = {
+          optional: [{ RtpDataChannels: true }],
+        };
+        // 这里就是需要的ICEServer了
+        var servers = {
+          iceServers: [
+            { urls: "stun:stun.services.mozilla.com" },
+            { urls: "stun:stun.l.google.com:19302" },
+          ],
+        };
+        var pc = new RTCPeerConnection(servers, mediaConstraints);
+        function handleCandidate(candidate) {
+          var ip_regex =
+            /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/;
+          var hasIp = ip_regex.exec(candidate);
+          if (hasIp) {
+            var ip_addr = ip_regex.exec(candidate)[1];
+            if (ip_dups[ip_addr] === undefined) callback(ip_addr);
+            ip_dups[ip_addr] = true;
+          }
+        }
+        // 网络协商的过程
+        pc.onicecandidate = function (ice) {
+          if (ice.candidate) {
+            handleCandidate(ice.candidate.candidate);
+          }
+        };
+        pc.createDataChannel("");
+        //创建一个SDP(session description protocol)会话描述协议 是一个纯文本信息 包含了媒体和网络协商的信息
+        pc.createOffer(
+          function (result) {
+            pc.setLocalDescription(
+              result,
+              function () {},
+              function () {}
+            );
+          },
+          function () {}
+        );
+        setTimeout(function () {
+          var lines = pc.localDescription.sdp.split("\n");
+          lines.forEach(function (line) {
+            if (line.indexOf("a=candidate:") === 0) handleCandidate(line);
+          });
+        }, 1000);
       }),
         (changeRouter = function (type, before, current) {
           rooterBefore = current;
@@ -1251,22 +1216,16 @@
             if (pro) {
               json.productName = pro;
             }
-            if(!json.ip){
-              getIPs(function (ip) {
-                baseJsonInfo.ip=ip;
-              });
-            }
             uploadActionHolder(json);
           }
         });
 
       return {
         init: function (project) {
-          if(!baseJsonInfo.ip){
-            getIPs(function (ip) {
-              baseJsonInfo.ip=ip;
-            });
-          }
+          getIPs(function (ip) {
+            baseJsonInfo.ip=ip;
+          });
+          setTimeout(() => {
             if (project) {
               localStorage.setItem("productName", project);
             }
@@ -1373,6 +1332,7 @@
               changeRouter("history", rooterBefore, window.location.href);
             });
             changeRouter("init", rooterBefore, window.location.href);
+          }, 2000);
         },
         setUploadListener(uploader) {
           uploadActionHolder = uploader;
